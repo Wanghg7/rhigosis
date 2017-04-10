a7ccdb2a2f36acaaa3304d6c3dfcd86627cd4285
wanghg <wanghg7@gmail.com>
2017-04-11 00:00:55 +0800

    parser: PROGRESS [  5/583] until call expr

--- src/test/scala/wanghg/rhigosis/ParserAgainstScalaLibraryTest.scala
+++ src/test/scala/wanghg/rhigosis/ParserAgainstScalaLibraryTest.scala
@@ -16,13 +16,17 @@ class ParserAgainstScalaLibraryTest {
 
   @Test
   def doTest(): Unit = {
+    val total = 583
+    var count = 1
     val it = Utils.files(List(Iterator(ROOT)), "scala")
     for (file <- it) {
+      printf("PROGRESS [%3d/%d] %s\n",count, total, file.getAbsolutePath)
       Utils.withReader(file) { reader =>
         val lexer = new Lexer(reader, file.getAbsolutePath)
         val parser = new Parser(lexer)
         parser.debug_parse()
       }
+      count = count + 1
     }
   }
 
