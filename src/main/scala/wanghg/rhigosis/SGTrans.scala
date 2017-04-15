396b58f07ffc4135d85192da749437084cf997d5
wanghg <wanghg7@gmail.com>
2017-04-15 16:54:37 +0800

    SGOutput

--- src/main/scala/wanghg/rhigosis/SGTrans.scala
+++ src/main/scala/wanghg/rhigosis/SGTrans.scala
@@ -1,6 +1,6 @@
 package wanghg.rhigosis
 
-import java.io.File
+import java.io.{File, StringReader}
 import java_cup.runtime.{ComplexSymbolFactory, SymbolFactory}
 
 import wanghg.Utils
@@ -9,14 +9,34 @@ import wanghg.Utils
   * Created by wanghg on 14/4/2017.
   */
 object SGTrans {
+
+  val fact = new ComplexSymbolFactory();
+
   def main(args: Array[String]): Unit = {
     val file = new File(args(0))
     require(file.exists())
-    val fact = new ComplexSymbolFactory();
     Utils.withReader(file) { reader =>
       val lexer = new SGLexer(reader, file.getPath, fact)
       val parser = new SGInputParser(lexer, fact)
-      parser.parse()
+      val transformed = transform(parser.parse().value.asInstanceOf[String])
+      val out = output(transformed)
+      print(out)
     }
   }
+
+  def transform(in: String): String = {
+    in
+  }
+
+  def output(in: String): String = {
+    val lexer = new SGLexer(new StringReader(in), "", fact)
+    val parser = new SGOutputParser(lexer, fact)
+    val out = parser.parse().value.asInstanceOf[String]
+    val sb = new StringBuilder
+    sb.append("package wanghg.rhigosis;\n\n")
+    sb.append(out)
+    sb.toString
+  }
+
 }
+
