b42b698b3002188096c6d2b9b4dfdf0e47e40f83
wanghg <wanghg7@gmail.com>
2017-04-16 23:09:32 +0800

    terminal

---
+++ src/main/scala/wanghg/rhigosis/SGUtils.scala
@@ -0,0 +1,75 @@
+package wanghg.rhigosis
+
+
+/**
+  * Created by wanghg on 16/4/2017.
+  */
+object SGUtils {
+
+  val ptn = "‘([^’]+)’".r
+
+  def createLiteral(s: String): Terminal = s match {
+    case ptn("-") => Terminal('MINUS)
+    case ptn("null") => Terminal('NULL)
+    case ptn(".") => Terminal('DOT)
+    case ptn(",") => Terminal('COMMA)
+    case ptn("this") => Terminal('THIS)
+    case ptn("super") => Terminal('SUPER)
+    case ptn("[") => Terminal('LBRACK)
+    case ptn("]") => Terminal('RBRACK)
+    case ptn("(") => Terminal('LPAREN)
+    case ptn(")") => Terminal('RPAREN)
+    case ptn("{") => Terminal('LBRACE)
+    case ptn("}") => Terminal('RBRACE)
+    case ptn("=>") => Terminal('RDARROW)
+    case ptn("forSome") => Terminal('FORSOME)
+    case ptn("type") => Terminal('TYPE)
+    case ptn("val") => Terminal('VAL)
+    case ptn("var") => Terminal('VAR)
+    case ptn("with") => Terminal('WITH)
+    case ptn("#") => Terminal('HASH)
+    case ptn(":") => Terminal('COLON)
+    case ptn("_") => Terminal('UNDERSCORE)
+    case ptn("*") => Terminal('ASTERISK)
+    case ptn("implicit") => Terminal('IMPLICIT)
+    case ptn("if") => Terminal('IF)
+    case ptn("else") => Terminal('ELSE)
+    case ptn("while") => Terminal('WHILE)
+    case ptn("try") => Terminal('TRY)
+    case ptn("catch") => Terminal('CATCH)
+    case ptn("finally") => Terminal('FINALLY)
+    case ptn("do") => Terminal('DO)
+    case ptn("for") => Terminal('FOR)
+    case ptn("yield") => Terminal('YIELD)
+    case ptn("throw") => Terminal('THROW)
+    case ptn("return") => Terminal('RETURN)
+    case ptn("=") => Terminal('EQ)
+    case ptn("match") => Terminal('MATCH)
+    case ptn("+") => Terminal('PLUS)
+    case ptn("~") => Terminal('TILDE)
+    case ptn("!") => Terminal('EXCLAM)
+    case ptn("new") => Terminal('NEW)
+    case ptn("lazy") => Terminal('LAZY)
+    case ptn("<-") => Terminal('LARROW)
+    case ptn("case") => Terminal('CASE)
+    case ptn("|") => Terminal('OR)
+    case ptn("@") => Terminal('AT)
+    case ptn(">:") => Terminal('LBOUNDS)
+    case ptn("<:") => Terminal('UBOUNDS)
+    case ptn("<%") => Terminal('VBOUNDS)
+    case ptn("override") => Terminal('OVERRIDE)
+    case ptn("abstract") => Terminal('ABSTRACT)
+    case ptn("final") => Terminal('FINAL)
+    case ptn("sealed") => Terminal('SEALED)
+    case ptn("private") => Terminal('PRIVATE)
+    case ptn("protected") => Terminal('PROTECTED)
+    case ptn("import") => Terminal('IMPORT)
+    case ptn("def") => Terminal('DEF)
+    case ptn("class") => Terminal('CLASS)
+    case ptn("object") => Terminal('OBJECT)
+    case ptn("trait") => Terminal('TRAIT)
+    case ptn("extends") => Terminal('EXTENDS)
+    case ptn("package") => Terminal('EXTENDS)
+  }
+
+}
---
+++ src/main/scala/wanghg/rhigosis/Terminal.scala
@@ -0,0 +1,6 @@
+package wanghg.rhigosis
+
+/**
+  * Created by wanghg on 16/4/2017.
+  */
+case class Terminal(sym: Symbol)
