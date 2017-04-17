5e91c541238a4655ad7a59e8551958da01fcf0ef
wanghg <wanghg7@gmail.com>
2017-04-17 19:15:58 +0800

    refactor

--- src/main/scala/wanghg/rhigosis/SGTrans.scala
+++ src/main/scala/wanghg/rhigosis/SGTrans.scala
@@ -18,27 +18,31 @@ object SGTrans {
     val file = new File(args(0))
     require(file.exists())
     val g: Grammar = parse(file)
-    val extracted = expandOptions(g, ListBuffer.empty[Prod])
+    val extracted = expandClause(g, ListBuffer.empty[Prod])
     println(extracted)
   }
 
-  def expandOptions(g: Grammar, acc: ListBuffer[Prod]): Grammar = {
+  def expandClause(g: Grammar, acc: ListBuffer[Prod]): Grammar = {
     g match {
       case Grammar(Nil) => Grammar(acc.toList)
-      case Grammar(p :: ps) => expandOptions(p, acc); expandOptions(Grammar(ps), acc)
+      case Grammar(p :: ps) => expandClause(p, acc); expandClause(Grammar(ps), acc)
     }
   }
 
-  def expandOptions(p: Prod, acc: ListBuffer[Prod]): Unit = {
+  def expandClause(p: Prod, acc: ListBuffer[Prod]): Unit = {
     p match {
       case Prod(nont, Options(Nil), false) =>
         ()
       case Prod(nont, Options(Nil), true) =>
         acc += Prod(nont, Options(Nil), false)
       case Prod(nont, Options(o :: os), nullable) =>
-        acc += Prod(nont, Options(List(o)), false)
-        expandOptions(Prod(nont, Options(os), nullable), acc)
+        expandSequence(nont, o.asInstanceOf[Sequence], acc)
+        expandClause(Prod(nont, Options(os), nullable), acc)
     }
+  }
+
+  def expandSequence(nont: Nonterminal, s: Sequence, acc: ListBuffer[Prod]): Unit = {
+    acc += Prod(nont, Options(List(s)), false)
   }
 
   def parse(file: File): Grammar = {
