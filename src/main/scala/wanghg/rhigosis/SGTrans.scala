734a337e942913e15a8def9d4dd2120284ef5ac9
wanghg <wanghg7@gmail.com>
2017-04-17 19:00:28 +0800

    expand options

--- src/main/scala/wanghg/rhigosis/SGTrans.scala
+++ src/main/scala/wanghg/rhigosis/SGTrans.scala
@@ -5,6 +5,8 @@ import java_cup.runtime.{ComplexSymbolFactory, SymbolFactory}
 
 import wanghg.Utils
 
+import scala.collection.mutable.ListBuffer
+
 /**
   * Created by wanghg on 14/4/2017.
   */
@@ -16,7 +18,27 @@ object SGTrans {
     val file = new File(args(0))
     require(file.exists())
     val g: Grammar = parse(file)
-    println(g)
+    val extracted = expandOptions(g, ListBuffer.empty[Prod])
+    println(extracted)
+  }
+
+  def expandOptions(g: Grammar, acc: ListBuffer[Prod]): Grammar = {
+    g match {
+      case Grammar(Nil) => Grammar(acc.toList)
+      case Grammar(p :: ps) => expandOptions(p, acc); expandOptions(Grammar(ps), acc)
+    }
+  }
+
+  def expandOptions(p: Prod, acc: ListBuffer[Prod]): Unit = {
+    p match {
+      case Prod(nont, Options(Nil), false) =>
+        ()
+      case Prod(nont, Options(Nil), true) =>
+        acc += Prod(nont, Options(Nil), false)
+      case Prod(nont, Options(o :: os), nullable) =>
+        acc += Prod(nont, Options(List(o)), false)
+        expandOptions(Prod(nont, Options(os), nullable), acc)
+    }
   }
 
   def parse(file: File): Grammar = {
